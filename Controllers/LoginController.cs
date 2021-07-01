49618ce1653db989ca2c967b0524bef5317778cc
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-07-01 11:19:29 -0700

    Added user authentication & authorization

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+        StudentManagementSystemEntities db = new StudentManagementSystemEntities();
+
+
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Post: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+            if(ModelState.IsValid)
+            {
+                using(StudentManagementSystemEntities db = new StudentManagementSystemEntities())
+                {
+                    var obj = db.users.Where(a => a.firstname.Equals(objchk.firstname) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["Firstname"] = obj.firstname.ToString();
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The username or password is incorrect, Please try again");
+                    }
+                }
+                
+            }
+
+            return View(objchk);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
